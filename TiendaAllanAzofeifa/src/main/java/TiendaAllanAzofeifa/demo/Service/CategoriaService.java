6197ba5c14c0f0ea316d21dff9877a67f85be11c
AllanAzo17 <aazofeifa60857@ufide.ac.cr>
2024-10-28 14:25:47 -0600

    semana 6
    Firebase y CRUD

--- TiendaAllanAzofeifa/src/main/java/TiendaAllanAzofeifa/demo/Service/CategoriaService.java
+++ TiendaAllanAzofeifa/src/main/java/TiendaAllanAzofeifa/demo/Service/CategoriaService.java
@@ -11,6 +11,16 @@ public interface CategoriaService {
     
     // Se obtiene un listado de categorias en un List
     public List<Categoria> getCategorias(boolean activos);
+    
+    // Se obtiene un Categoria, a partir del id de una categoria
+    public Categoria getCategoria(Categoria categoria);
+    
+    // Se inserta un nueva categoria si el id de la categoria esta vacio
+    //Se actualiza una categoria si el id de la categoria NO esta vacia
+    public void save(Categoria categoria);
+    
+    // Se elimina la categoria que tiene el id pasado por parámetro
+    public void delete(Categoria categoria);
  
 }
 
